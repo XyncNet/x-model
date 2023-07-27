cd2d5e9f0c70988f3ec32daea00600d0ee553ca6
Artemiev <mixartemev@gmail.com>
2023-07-27 12:56:45 +0300

    geometry fixes

--- tortoise_api_model/model.py
+++ tortoise_api_model/model.py
@@ -4,6 +4,7 @@ from tortoise.fields import Field
 
 
 class Model(BaseModel):
+    # id: int = IntField(pk=True)
     _name: str = 'name'
     def repr(self):
         if self._name in self._meta.db_fields:
@@ -14,9 +15,20 @@ class Model(BaseModel):
 # Custom Fields
 class PointField(Field[Point]):
     SQL_TYPE = "POINT"
-    field_type = tuple[float, float]
+    field_type = Point
+
+    def to_python_value(self, value):
+        if value is not None and not isinstance(value, Point):
+            value = Point(*value)  # pylint: disable=E1102
+        self.validate(value)
+        return value
 
 class PolygonField(Field[Polygon]):
     SQL_TYPE = "POLYGON"
-    field_type = tuple[tuple[float, float]]
+    field_type = Point
 
+    def to_python_value(self, value):
+        if value is not None and not isinstance(value, Polygon):
+            value = Polygon(*value)  # pylint: disable=E1102
+        self.validate(value)
+        return value
