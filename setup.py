3070e7daeb86dd514a987cdb172e2c08f0ba239b
artemiev <mixartemev@gmail.com>
2024-03-19 15:06:17 +0300

    pyds

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.7.5'
+VERSION = '0.7.6'
 DESCRIPTION = 'Base model for tortoise-api'
 LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
 
