f42142bd31591c35e338dbc6679bd58c0d08f2e0
Mike <mixartemev@gmail.com>
2024-10-11 21:37:47 +0300

    env

--- tests/test_db.py
+++ tests/test_db.py
@@ -7,6 +7,8 @@ from tortoise_api_model import init_db, model
 
 load_dotenv()
 
+PG_DSN = f"postgres://{env('POSTGRES_USER')}:{env('POSTGRES_PASSWORD')}@{env('POSTGRES_HOST', 'xyncdbs')}:{env('POSTGRES_PORT', 5432)}/{env('POSTGRES_DB', env('POSTGRES_USER'))}"
+
 
 def test_init_db():
-    assert isinstance(run(init_db(env("DB_URL"), model)), AsyncpgDBClient), "DB corrupt"
+    assert isinstance(run(init_db(PG_DSN, model)), AsyncpgDBClient), "DB corrupt"
