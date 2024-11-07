30b494b70800fb71795520d479c25992ade281bc
Mike <mixartemev@gmail.com>
2024-11-07 14:00:13 +0300

    FULL REFACT

--- x_model/pydantic.py
+++ x_model/pydantic.py
@@ -1,14 +1,14 @@
-from typing import TypeVar, Generic
-from pydantic import BaseModel, ConfigDict
+# from typing import TypeVar, Generic
+from pydantic import BaseModel  # , ConfigDict
 
 
-RootModelType = TypeVar("RootModelType")
-
-
-class PydList(BaseModel, Generic[RootModelType]):
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-    data: list[RootModelType]
-    total: int
+# RootModelType = TypeVar("RootModelType")
+#
+#
+# class PydList(BaseModel, Generic[RootModelType]):
+#     model_config = ConfigDict(arbitrary_types_allowed=True)
+#     data: list[RootModelType]
+#     total: int
 
 
 class Names(BaseModel):
