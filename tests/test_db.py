190b84b2cbc6af41adacd47e75d3f10ed37bd529
xync <mixartemev@gmail.com>
2025-06-11 22:23:55 +0300

    fix: orm-conf

--- tests/test_db.py
+++ tests/test_db.py
@@ -9,6 +9,13 @@ load_dotenv()
 
 PG_DSN = f"postgres://{env('POSTGRES_USER')}:{env('POSTGRES_PASSWORD')}@{env('POSTGRES_HOST', 'xyncdbs')}:{env('POSTGRES_PORT', 5432)}/{env('POSTGRES_DB', env('POSTGRES_USER'))}"
 
+TORM = {
+    "connections": {"default": PG_DSN},
+    "apps": {"models": {"models": [models]}},
+    "use_tz": False,
+    "timezone": "UTC",
+}
+
 
 def test_init_db():
-    assert isinstance(run(init_db(PG_DSN, models)), AsyncpgDBClient), "DB corrupt"
+    assert isinstance(run(init_db(TORM)), AsyncpgDBClient), "DB corrupt"
--- x_model/__init__.py
+++ x_model/__init__.py
@@ -1,11 +1,9 @@
-from types import ModuleType
-
 from tortoise import Tortoise, connections
 from tortoise.backends.asyncpg import AsyncpgDBClient
 
 
-async def init_db(dsn: str, models: ModuleType, create_tables: bool = True) -> AsyncpgDBClient | str:
-    await Tortoise.init(db_url=dsn, modules={"models": [models]})
+async def init_db(conf: dict, create_tables: bool = True) -> AsyncpgDBClient | str:
+    await Tortoise.init(conf)
     if create_tables:
         await Tortoise.generate_schemas()
     cn: AsyncpgDBClient = connections.get("default")
