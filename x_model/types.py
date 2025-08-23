7c9cfe7e3fca9077a8109e9265f8826d9d1178bd
xync <mixartemev@gmail.com>
2025-08-23 14:45:57 +0300

    fix: add kwargs

--- x_model/types.py
+++ x_model/types.py
@@ -6,6 +6,6 @@ from typing import ClassVar
 class BaseUpd:
     _unq: ClassVar[set[str]]
 
-    def df_unq(self) -> dict:
-        d = {k: v for k, v in asdict(self).items() if v is not None or k in self._unq}
+    def df_unq(self, **kwargs) -> dict:
+        d = {k: v for k, v in (asdict(self) | kwargs).items() if v is not None or k in self._unq}
         return {**{k: d.pop(k, None) for k in self._unq}, "defaults": d}
