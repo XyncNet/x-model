654dc95cf1d1f41f025ed95b539740c26f407574
xync <mixartemev@gmail.com>
2026-02-03 16:31:06 +0700

    fix: unq

--- x_model/__init__.py
+++ x_model/__init__.py
@@ -2,9 +2,10 @@ from tortoise import Tortoise, connections
 from tortoise.backends.asyncpg import AsyncpgDBClient
 
 
-async def init_db(conf: dict, create_tables: bool = True) -> AsyncpgDBClient | str:
+async def init_db(conf: dict, create_tables: bool = False) -> AsyncpgDBClient | str:
     await Tortoise.init(conf)
-    if create_tables:
-        await Tortoise.generate_schemas()
     cn: AsyncpgDBClient = connections.get("default")
+    if create_tables:
+        await cn.execute_script("CREATE EXTENSION IF NOT EXISTS uint128;")
+        await Tortoise.generate_schemas()
     return cn
